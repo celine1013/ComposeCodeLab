4ac0a7a34c4281db68b97fe761a8750be8371004
Celine Liu <celine@demandmanager.com.au>
2021-04-16 16:57:16 +1000

    color, gradle

--- app/src/main/java/com/example/composecodelab/ui/theme/Color.kt
+++ app/src/main/java/com/example/composecodelab/ui/theme/Color.kt
@@ -6,3 +6,14 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val DeepPurple50 = Color(0xffede7f6)
+val Pink50 = Color(0xfffce4ec)
+val DeepPurple900 = Color(0xff311b92)
+val DeepPurple900Light = Color(0xff6746c3)
+val DeepPurple900Dark = Color(0xff000063)
+val Red900 = Color(0xffb71c1c)
+val Red900Light = Color(0xfff05545)
+val Red900Dark = Color(0xff7f0000)
+
+
